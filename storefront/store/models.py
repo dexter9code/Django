967da2f988924fb0f6dae0031f7700043985311c
rudy <doomdexter07@gmail.com>
2022-01-31 12:38:34 +0530

    remove the meta class

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -50,11 +50,6 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
-    class Meta:
-        db_table = 'store.customers'
-        indexes = [models.Index(fields=['last_name', 'first_name'])
-                   ]
-
 
 class Order(models.Model):
     STATUS_PEND = 'P'
