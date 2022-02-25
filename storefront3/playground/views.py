dda378eb8864d2eb1e8d6117d3496d2fade124a9
rudy <doomdexter07@gmail.com>
2022-02-25 10:20:39 +0530

    create a fake SMTP server to send mail

--- storefront3/playground/views.py
+++ storefront3/playground/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
 
 
 def say_hello(request):
+    try:
+        send_mail('subject', 'this is first plain text message',
+                  'info@valorant.com', ['bob@buidler.com'])
+    except:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
