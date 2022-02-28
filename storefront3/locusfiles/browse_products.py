d45edb055888fcba610b6fe638b7de59b756f941
rudy <doomdexter07@gmail.com>
2022-02-28 09:22:14 +0530

    create performace test using locust

---
+++ storefront3/locusfiles/browse_products.py
@@ -0,0 +1,24 @@
+from locust import HttpUser, between, task
+from random import randint
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task(2)
+    def view_collection(self):
+        collection_id = randint(2, 6)
+        self.client.get(
+            f'/store/products/?collection_id={collection_id}', name='/store/products')
+
+    @task(4)
+    def view_products(self):
+        product_id = randint(1, 1000)
+        self.client.get(
+            f'/store/products/{product_id}', name='/store/products/:id')
+
+    @task(1)
+    def add_to_cart(self):
+        product_id = randint(1, 10)
+        self.client.post(f'/store/carts/{self.cart_id}/items/', name='/store/carts/items', json={
+                         'product_id': product_id, 'quality': 1})
